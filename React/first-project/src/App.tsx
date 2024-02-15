4e51d58acc8c129d54334e91b13fc9c1619c5e70
Leo <leonardo.r.s.duarte@gmail.com>
2024-02-15 17:23:02 -0300

    Atividade props e hooks - 15/02/2024

--- React/first-project/src/App.tsx
+++ React/first-project/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './pages/home/Home';
 import './App.css';
 
 function App() {
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
+
   return (
-    <Home />
+    <>
+      <Home
+        title="Componente Home"
+        description="Este é um componente Home que recebe props."
+      />
+
+      <img src="https://i.imgur.com/H88yIo2.png" className='img' alt="Imagem tela inicial"/> 
+
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </>
   );
 }
 
--- React/first-project/src/pages/home/Home.tsx
+++ React/first-project/src/pages/home/Home.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 
-function home() {
+interface myProps {
+    title: string;
+    description: string;
+}
+
+function Home(props:myProps) {
+    const [completed, setCompleted] = useState(false);
+    const [tarefa, setTarefa] = useState('');
+
+    useEffect(() => {
+        if (completed) {
+            setTarefa('Parabéns! Você concluiu a tarefa!');
+        }
+    }, [completed]); 
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
     return (
-        <>
-            <h1 className='titulo'>Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" className='img' alt="Imagem tela inicial" />
-        </>
+        <div className="container">
+            {loggedIn ? (
+                <>
+                    <div> 
+                        <h1 className='titulo'>{props.title}</h1>
+                        <p>{props.description}</p>
+                    </div>
+                    
+                    <div>
+                        <h2>Tarefa</h2>
+                        <h4>{tarefa}</h4>
+                        <button onClick={() => setCompleted(true)}>Concluir tarefa</button>
+                    </div>
+                </>
+            ) : (
+                <button onClick={() => setLoggedIn(true)}>Entrar</button>
+            )} 
+        </div>
     );
 }
 
-export default home;
+export default Home;
