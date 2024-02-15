2b4cb34e1c27b9e203ffd5f0a6f113c6443c4486
Leo <leonardo.r.s.duarte@gmail.com>
2024-02-15 11:34:19 -0300

    First project in React

---
+++ React/first-project/src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function home() {
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" className='img' alt="Imagem tela inicial" />
+        </>
+    );
+}
+
+export default home;
