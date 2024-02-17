8a4673788b10b28b60842e8ae9daa7b5e7757dc5
Leo <leonardo.r.s.duarte@gmail.com>
2024-02-17 12:22:10 -0300

    React Router DOM - Projeto routerDom

---
+++ React/routerDom/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
